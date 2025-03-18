93a9adc5ea7d49ecbe2e0c31d8c0604cef77474f
KwangryeolPark <pkr7098@unist.ac.kr>
2025-03-18 13:27:56 +0900

    update

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-with open('README.md', encoding='utf-8') as f: # README.md 내용 읽어오기
-	  long_description = f.read()
+with open('README.md', encoding='utf-8') as f:
+	long_description = f.read()
 
 setup(
-	name='HelloWorld', #module 이름
-	version='0.0.0.1', # 버전 등록
-	long_description    = long_description, # readme.md 등록
-	long_description_content_type = 'text/markdown',  # readme.md 포맷
-	description='hello world 패키지이다.', # 패키지 설명
-	author='', # 참여자 등록
-	author_email='', # 이메일 등록
-	url='', # url 등록
-	license='MIT', # 라이센스 등록
-	python_requires='>=3.4', #파이썬 버전 등록
-	install_requires=[ 'boto3', 'pymongo'], # module 필요한 다른 module 등록
-	packages=['data_lab'] # 업로드할 module이 있는 폴더 입력
+	name='PPT',
+	version='0.0.1',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	description='PPT Debugging version',
+	author='Jaeho Kim, Kwangryeol Park',
+	author_email='kjh3690@unist.ac.kr, pkr7098@unist.ac.kr',
+	url='https://github.com/jaeho3690/PPT_PIP',
+	license='MIT',
+	python_requires='>=3.4',
+	install_requires=['torch', 'einops', 'numpy<2.0', 'tqdm'],
+	packages=find_packages(),
+ 	keywords=['PPT', 'Permutation', 'Pytorch', 'Transformer', 'Time-series', 'Self-supervised'],
 )
